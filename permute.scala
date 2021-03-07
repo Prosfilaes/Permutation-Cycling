4d48d93b4094c98a3c012edcae6ad0440869a5ee
David Starner <prosfilaes@gmail.com>
2021-03-06 23:42:13 -0800

    working fast version

--- permute.scala
+++ permute.scala
@@ -1,7 +1,9 @@
 import scala.collection.parallel.CollectionConverters._
+import java.util.concurrent.atomic.AtomicBoolean
 
 package Permute {
   object Permute {
+    
     type Perm = Vector[Int]
     def permToString (p : Perm) : String = {
       p.map (_.toString).reduce (_ ++ _)
@@ -9,14 +11,19 @@ package Permute {
     def perm (n : Int) : Option[List[Perm]] = {
       val start = Range(1, n + 1).toVector
       val length = Range (1, n + 1).product
+      val abort = new AtomicBoolean (false)
       
       def k (l : List[Perm], s : Set[Perm]) : Option[List[Perm]] = {
 
         def swap (a : Perm, b : Int) =
           a.updated(b - 1, a(b)).updated(b, a(b-1))
 
-        if (l.size == length) {
+        if (abort.get()) {
+          None
+        }
+        else if (l.size == length) {
           println ("!" + l.map(permToString).toString)
+          abort.set(true)
           Some(l)
         }
         else if (l.size == 1) {
